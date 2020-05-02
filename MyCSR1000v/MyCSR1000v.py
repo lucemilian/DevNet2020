05a320e1403b100132c35197f57482b560ca5d8e
le.ghergheluca <le.ghergheluca@alumnos.upm.es>
2020-05-02 20:29:48 +0200

    Creado esqueleto MyCSR1000v y actualizado ReadME

---
+++ MyCSR1000v/MyCSR1000v.py
@@ -0,0 +1,120 @@
+import sys
+import requests
+import time
+import json
+import urllib3
+from time import sleep
+
+#El texto se ha puesto en ingles con el objetivo de practicar el idioma
+
+#Mensaje de bienvenida
+startMsg = " STARTING MyCSR1000v" 
+print(startMsg.center(50,"$"))
+
+#Credenciales
+username="cisco"
+password="cisco123!"
+sTicket=""
+
+# Funcionalidad para OBTENER listado de las interfaces del router
+def routerINTget():
+
+    fN0Msg = " I am: " + routerINTget.__name__+" "
+    print(fN0Msg.center(40,"*"))
+    
+    
+    sleep(3)
+    return
+
+# Funcionalidad para CREAR interfaces del router
+def routerINTmake():
+
+    fN1Msg = " I am: " + routerINTmake.__name__+" "
+    print(fN1Msg.center(40,"*"))
+    
+    
+    sleep(3)
+    return
+
+# Funcionalidad para BORRAR interfaces del router
+def routerINTdestroy():
+
+    fN2Msg = " I am: " + routerINTdestroy.__name__+" "
+    print(fN2Msg.center(40,"*"))
+    
+    
+    sleep(3)
+    return
+
+# Funcionalidad para manipular tabla de enrutado del router
+def routerROUTING():
+
+    fN3Msg = " I am: " + routerROUTING.__name__+" "
+    print(fN3Msg.center(40,"*"))
+    
+
+    
+    sleep(3)
+    return
+
+
+# Funcionalidad para realizar petición a 2 módulos de yang diferentes
+def routerTOyang():
+
+    fN4Msg = " I am: " + routerTOyang.__name__+" "
+    print(fN4Msg.center(40,"*"))
+    
+    
+    
+    sleep(3)
+    return
+
+
+
+
+def bye():
+    sys.exit("Exiting...")
+
+switcher = {
+        0: routerINTget,
+        1: routerINTmake,
+        2: routerINTdestroy,
+        3: routerROUTING,
+        4: routerTOyang,
+        5: bye
+    }
+
+# El buen main...menú
+def main():
+
+    mainMsg=" MAIN MENU "
+
+    requests.packages.urllib3.disable_warnings()
+
+    while True:         
+        print("\n",mainMsg.center(40,"*"))
+        
+        print ("-[0] Get list of router's interfaces",
+        "\n-[1] Create interface",
+        "\n-[2] Delete interface",
+        "\n-[3] Configure routing table",
+        "\n-[4] Call other modules to YANGOUT",
+        "\n-[5] EXIT")
+        vOpera = input("Using numbers, specify the desired operation: ")
+        
+        print ("\nProcessing...\n")
+        
+        try:
+            func = switcher.get(int(vOpera),)
+            func()
+
+        except ValueError as e:
+            print(e)
+            print ("\nINVALID OPTION!\n")
+            pass
+        
+        pass
+    
+
+if __name__ == "__main__":
+    main()
--- README.md
+++ README.md
@@ -6,20 +6,30 @@ Becas Digitaliza: DevNet - Ene 20
 
 - [x] Crear una cuenta de GitHub y, al menos, un repositorio público.
 
+>![avatar]: Repositiorio incialmente privado, pasado a público en cuanto el tutor pida acceso.
+------
 - [x] Calculadora (es una tarea a la que le añadiremos más funcionalidades).
     - [x] Crear un menú que indique las opciones que hay.
     - [x] Como mínimo, incluir: suma, resta, multiplicación, división, exponenciales y raíces cuadradas. Se pueden añadir más funcionalidades.
     - Podéis usar las funciones y técnicas vistas en las sesiones, o implementar otras que consideréis oportunas. Obviamente, ser eficientes tendrá más nota.
 
+>![avatar]: Calculadora creada, con nombre "MathPy".
+-----
 - [x] Crear un programa que permita conectarse con el controlador APIC-EM de Cisco
     - [x] El usuario tendrá que escoger la opción que quiera (no tendrá que especificar la url a mano)
     - [x] Añadir, como mínimo, 4 funcionalidades
 
-- [ ] Instalar el laboratorio de la máquina virtual el router CSR1000v.
+>![avatar]: Programa creado, con nombre "APICEM.EXE".
+----
+- [X] Instalar el laboratorio de la máquina virtual el router CSR1000v.
     > Os he añadido el anuncio "Instalación Máquina Virtual - Cisco Cloud Services Router 1000V Series", el cual os describe los pasos a seguir y dónde están los archivos
+
     > [Esta actividad no se evaluará como tal, pero la incluyo aquí porque hay que preparar el entorno (o el Sandbox de cisco.developer.com) para poder hacer la tarea siguiente].
 
-- [ ] Crear un script que permita conectarnos a nuestra Router CSR1000v (bien sea en local o a través del Sandbox) y que, a través de un menú, nos aparezcan una serie de opciones que nos permita realizar las siguientes tareas:
+>![avatar]: Más fácil de lo que parecía.
+
+----
+- [x] Crear un script que permita conectarnos a nuestra Router CSR1000v (bien sea en local o a través del Sandbox) y que, a través de un menú, nos aparezcan una serie de opciones que nos permita realizar las siguientes tareas:
     - [ ] Obtener un listado de las interfaces del router (indicar, en modo tabla, el nombre de la interfaz, su IP y MAC)
     - [ ] Crear Interfaces
     - [ ] Borrar Interfaces
@@ -28,3 +38,5 @@ Becas Digitaliza: DevNet - Ene 20
 
 
 - - - -
+
+[avatar]: https://avatars0.githubusercontent.com/u/20265786?s=60&u=7fbaefdf4b1a1e7f87c3e8ec411d99a18eb76319&v=4
