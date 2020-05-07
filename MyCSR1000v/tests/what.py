52fb53758b66157c23a51d430d2d2ed02cbfa8de
le.ghergheluca <le.ghergheluca@alumnos.upm.es>
2020-05-07 21:30:40 +0200

    Cleaning

--- MyCSR1000v/tests/what.py
+++
@@ -1,9 +0,0 @@
-counter=0
-for i in range(0,5):
-        counter+=1
-        if counter == 0:
-            print("-".center(40,"-"))
-            print('{:<10s}{:>4s}{:>12s}{:>12s}'.format("Name","IP Address","NetMask")
-            print("caca")
-        else:
-            print("cac")
